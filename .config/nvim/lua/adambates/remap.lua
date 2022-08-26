f32cba7e9a1c2ff6fd4c3b37481e92d8bc5cb7cf
adam-bates <adam@adambates.ca>
2022-08-26 11:42:47 -0400

    Updated navigation to make block and page jumps easier.

--- .config/nvim/lua/adambates/remap.lua
+++ .config/nvim/lua/adambates/remap.lua
@@ -7,6 +7,11 @@ local inoremap = keymap.inoremap
 
 inoremap("<esc><CR>", "<esc>o")
 
+nnoremap("[", "{")
+nnoremap("]", "}")
+nnoremap("{", "<c-u>")
+nnoremap("}", "<c-d>")
+
 nnoremap("<leader>w", "<cmd>w<CR>")
 nnoremap("<leader>q", "<cmd>q<CR>")
 
